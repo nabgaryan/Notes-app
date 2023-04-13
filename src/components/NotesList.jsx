508cc0648b8425e3f9fbf05f8eb37d56a9482071
Norayr <norayr.abgaryan@gmail.com>
2023-04-13 16:39:53 +0400

    8 commit

--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,20 +1,46 @@
 import AddNote from "./AddNote";
 import Note from "./Note";
+import { useEffect, useState } from "react";
 
 const NotesList = ({ notes, onAddNote, onDeleteNote }) => {
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  function getWindowSize() {
+    const { innerWidth, innerHeight } = window;
+    return { innerWidth, innerHeight };
+  }
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  const width = windowSize.innerWidth;
+
   return (
-      <div className={notes.length <= 1 ? `notes-list-` : "notes-list"}>
-        {notes.map((note) => (
-          <Note
-            onDeleteNote={onDeleteNote}
-            key={note.id}
-            id={note.id}
-            text={note.text}
-            date={note.date}
-          />
-        ))}
-        <AddNote onAddNote={onAddNote} />
-      </div>
+    <div
+      className={
+        notes.length <= 1 || width < 600 ? `notes-list-` : "notes-list"
+      }
+    >
+      <AddNote onAddNote={onAddNote} />
+      {notes.map((note) => (
+        <Note
+          onDeleteNote={onDeleteNote}
+          key={note.id}
+          id={note.id}
+          text={note.text}
+          date={note.date}
+        />
+      ))}
+    </div>
   );
 };
 
